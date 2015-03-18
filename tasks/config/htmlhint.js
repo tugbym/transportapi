f3dbb52de44d4a4bbdda095a7f99bf6babc81c9c
tugbym <tugbym@uni.coventry.ac.uk>
2015-03-18 03:40:33 +0000

    Installed forever to keep the server running

--- tasks/config/htmlhint.js
+++ tasks/config/htmlhint.js
@@ -1,20 +1,16 @@
 module.exports = function(grunt) {
-
-	grunt.config.set('htmlhint', {
-        build: {
-            options: {
-                'tag-pair': true,
-                'tagname-lowercase': true,
-                'attr-lowercase': true,
-                'attr-value-double-quotes': true,
-                'doctype-first': true,
-                'spec-char-escape': true,
-                'id-unique': true,
-                'style-disabled': true
-            },
-            src: ['./assets/**/*.html']
-        }
-	});
-
-	grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.config.set('htmlhint', {
+        options: {
+            'tag-pair': true,
+            'tagname-lowercase': true,
+            'attr-lowercase': true,
+            'attr-value-double-quotes': true,
+            'doctype-first': true,
+            'spec-char-escape': true,
+            'id-unique': true,
+            'style-disabled': true
+        },
+        src: ['./assets/**/*.html']
+    });
+    grunt.loadNpmTasks('grunt-htmlhint');
 };
--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -1,5 +1,6 @@
 module.exports = function (grunt) {
 	grunt.registerTask('prod', [
+        'htmlhint',
 		'compileAssets',
 		'concat',
 		'uglify',
